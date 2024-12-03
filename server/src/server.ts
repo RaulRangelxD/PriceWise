40af75b14b99c5d9d39c0ca8f2d056307ed42859
RaulRangelxD <raulrangel1230@gmail.com>
2024-12-03 19:05:28 -0400

    vercel :c

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,41 @@
-import Server from './controllers/app.js'
-import * as v from './global/var.js'
 import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+import * as v from './global/var.js'
+import { authRouter, otpsRouter, usersRouter } from './routes/index.js'
+import { createTables, deleteTables } from './config/database.js'
 
-class InitServer {
-  #port = v.PORT || 4000
-  app: express.Application
+dotenv.config()
 
-  constructor() {
-    this.app = new Server().createApp()
-  }
+const app = express()
 
-  createServer() {
-    this.app.listen(this.#port, () => {
-      this.listener()
-    })
-  }
+const origins = v.ORIGINS || [`http://localhost:${v.PORT}`, `http://localhost:3000`]
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(
+  cors({
+    origin: origins,
+    credentials: true,
+  })
+)
+app.use(cookieParser())
 
-  listener() {
-    console.log(`Server listening on PORT: http://localhost:${this.#port}`)
-  }
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.use('/auth', authRouter)
+app.use('/users', usersRouter)
+app.use('/otps', otpsRouter)
+
+createTables()
+
+if (!process.env.VERCEL) {
+  const port = process.env.PORT || 4000
+  app.listen(port, () => {
+    console.log(`Server listening on PORT: http://localhost:${port}`)
+  })
 }
 
-new InitServer().createServer()
+export default app
